b55a2f5e63c604bde879c9c25cfe265865f1d918
Nikita <gnikita9999@gmail.com>
2026-06-26 16:54:36 +0300

    Update auth-config: apply new password hashes from secrets
    Added last updated date for passwords in comments.

--- auth-config.js
+++ auth-config.js
@@ -2,5 +2,6 @@
 // Hashes are SHA-256 of passwords, set via GitHub Secrets
 // Default hashes: admin=admin123, viewer=viewer123
 // To change passwords: update ADMIN_PASSWORD_HASH and VIEWER_PASSWORD_HASH in repo secrets
+// Passwords last updated: 2026-06-26
 window.ADMIN_HASH = '__ADMIN_HASH__';
 window.VIEWER_HASH = '__VIEWER_HASH__';
